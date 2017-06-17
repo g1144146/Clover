8dc9981ec4ef538629f1d51e3bfc4207fc3262bf
稲垣賢一 <das.nfd.gmw@gmail.com>
2017-06-17 09:06:27 +0900

    commit.

--- src/main/kotlin/clover/Clover.kt
+++ src/main/kotlin/clover/Clover.kt
@@ -1,7 +1,28 @@
 package clover
 
+import java.nio.file.Files
+import java.nio.file.Paths
+import org.objectweb.asm.ClassReader as Reader
+import org.objectweb.asm.ClassWriter as Writer
+import org.objectweb.asm.tree.MethodNode
+
 class Clover(args: Array<String>) {
     init {
-
+        try {
+            args.map { Paths.get(it)          }
+                .map { Files.readAllBytes(it) }
+                .map { Reader(it)             }
+                .map {
+                    val visitor: CloverClassNode = CloverClassNode()
+                    it.accept(visitor, 0)
+                    visitor
+                }
+                .forEach {
+                    val methods: List<MethodNode> = it.methods as List<MethodNode>
+                    methods.map { it as CloverMethodNode }
+                }
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
